897a11efadd6ab2d4d56fab48f75fbf4d78354c8
AhmedBerrais <ahmed.berrais@fsb.ucar.tn>
2025-01-11 11:30:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # Set your password here
 PASSWORD = "ahmed"
-hf_token = os.getenv("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")
+hf_token = os.getenv("HF_TOKEN")
 # Load the Hugging Face model and tokenizer
 model_path = "Tatakaiiii/Mouto"  # Your Hugging Face model
 tokenizer = AutoTokenizer.from_pretrained(model_path)
